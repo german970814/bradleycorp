34db2e07dcb00b2f70f35531d4889e6f78b85f88
Mario Vallejo <mario@vallgroup.com>
2018-01-20 13:58:21 -0600

    added icons and their styles to the compliance tab

--- src/containers/Pages/ProductDetail/ProductTabs/Tabs/TabCompliance/TabCompliance.js
+++ src/containers/Pages/ProductDetail/ProductTabs/Tabs/TabCompliance/TabCompliance.js
@@ -9,45 +9,101 @@ class TabCompliance extends Component {
     super(props)
 
     this.renderCompliance = this.renderCompliance.bind(this)
+    this.renderComplianceLinks = this.renderComplianceLinks.bind(this)
+    this.renderComplianceIcons = this.renderComplianceIcons.bind(this)
+    this.renderComplianceIconsHtml = this.renderComplianceIconsHtml.bind(this)
   }
 
   renderCompliance () {
     if ( this.props.compliance.compliance.length ) {
-      return this.props.compliance.compliance.map((compliance, index) => {
-        return (
-          <li
-            key={index} >
-            <FileDownloadLink
-              title={compliance.post['post_title'] || ''}
-              link={compliance.meta['technical_info_pdf']}
-              titleClass={tabStyle.tabTextOrange}
-              linkClass={tabStyle.tabTextOrangeLink}
-              iconClass={tabStyle.wordPDFIcon} />
-          </li>
-        )
-      })
+      return (
+        <div
+          className={tabStyle.halfWidthColDesktopTab} >
+          <h5
+            className={tabStyle.tabColTitle}>
+            {'Compliance'}
+          </h5>
+          <ul
+            className={tabStyle.tabColUl} >
+            {this.renderComplianceLinks()}
+          </ul>
+        </div>
+      )
     }
+  }
+
+  renderComplianceLinks () {
+    return this.props.compliance.compliance.map((compliance, index) => {
+      return (
+        <li
+          key={index} >
+          <FileDownloadLink
+            title={compliance.post['post_title'] || ''}
+            link={compliance.meta['technical_info_pdf']}
+            titleClass={tabStyle.tabTextOrange}
+            linkClass={tabStyle.tabTextOrangeLink}
+            iconClass={tabStyle.wordPDFIcon} />
+        </li>
+      )
+    })
+  }
+
+  renderComplianceIcons () {
+    if ( this.props.compliance.compliance_icons.length ) {
+      return (
+        <div
+          className={tabStyle.halfWidthColDesktopTab} >
+
+          <ul
+            className={tabStyle.tabColUl} >
+            {this.renderComplianceIconsHtml()}
+          </ul>
+        </div>
+      )
+    }
+  }
+
+  renderComplianceIconsHtml () {
+    return this.props.compliance.compliance_icons.map((icon, index) => {
+      if ( 'undefined' === typeof icon.media
+        && ! icon.media.featured_image.length ) {
+        return;
+      }
+      return (
+        <li
+          key={index}
+          className={style.complianceIcon}>
+          {(icon.meta && icon.meta.compliance_link.length)
+            ? <a
+              href={icon.meta.compliance_link}
+              className={style.complianceLink}
+              target="_blank" >
+              <img
+              src={icon.media.featured_image[0]} />
+            </a>
+            : <img
+              src={icon.media.featured_image[0]} />}
+        </li>
+      )
+    })
   }
 
   render () {
     return (
       <div
-        className={[style.tabCompliance, tabStyle.halfWidthColDesktopTab].join(' ')} >
-        <h5
-          className={tabStyle.tabColTitle}>
-          {'Compliance'}
-        </h5>
-        <ul
-          className={tabStyle.tabColUl} >
-          {this.renderCompliance()}
-        </ul>
+        className={style.tabCompliance} >
+
+        {this.renderCompliance()}
+
+        {this.renderComplianceIcons()}
+
       </div>
     )
   }
 }
 
 TabCompliance.propTypes = {
-  compliance: PropTypes.array.isRequired
+  compliance: PropTypes.object.isRequired
 }
 
 export default TabCompliance
