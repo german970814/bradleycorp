cc2b6147aced482d44692a52427b88a99276a71c
mattwills8 <matt_wills8@hotmail.co.uk>
2018-01-23 17:31:05 +0000

    style tablet header

--- src/components/Elements/Header/Header.js
+++ src/components/Elements/Header/Header.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Media from 'react-media'
 import { MOBILEMAXWIDTH, TABLETMAXWIDTH } from '../../../globals'
 import HeaderMobile from './HeaderMobile/HeaderMobile'
+import HeaderTablet from './HeaderTablet/HeaderTablet'
 
 const Header = ({ menuItems }) => {
   return (
@@ -17,11 +18,11 @@ const Header = ({ menuItems }) => {
             {match =>
               match ? (
                 // tablet
-                <HeaderMobile
+                <HeaderTablet
                   menuItems={menuItems} />
               ) : (
                 // desktop
-                <HeaderMobile
+                <HeaderTablet
                   menuItems={menuItems} />
               )
             }
