4cf0bdb64973410cc4d33c84418ef2507a2fdfcf
mattwills8 <matt_wills8@hotmail.co.uk>
2018-01-25 19:57:28 +0000

    finish footer styling

---
+++ src/components/Elements/Footer/FooterBottomSection/LoginItems.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import FooterBottomSectionItem from './FooterBottomSectionItem'
+
+const LoginItems = props => {
+  return (
+    <React.Fragment>
+
+      <FooterBottomSectionItem
+        link={'#'}
+        title={'LOGIN'} />
+
+      <FooterBottomSectionItem
+        link={'#'}
+        title={'ANSWERNET'} />
+
+      <FooterBottomSectionItem
+        link={'#'}
+        title={'BRADZONE'} />
+
+    </React.Fragment>
+  )
+}
+
+export default LoginItems
