1723dd054f23e36ca93635157e25464d20062b0b
mattwills8 <matt_wills8@hotmail.co.uk>
2018-02-28 19:06:40 -0800

    update logic for getting main menus to get by location slug rather than menu name

--- src/apps/Bradley/App/App.js
+++ src/apps/Bradley/App/App.js
@@ -10,18 +10,17 @@ class App extends Component {
   constructor (props) {
     super(props)
 
-    const defaults = {
+    this.defaults = {
       primaryMenu: [],
       footer: {
-        menu_1: [],
-        menu_2: [],
-        menu_3: [],
+        menu1: [],
+        menu2: [],
+        menu3: [],
         social_media_icons: []
       }
     }
 
-    this.defaults = defaults
-    this.state = defaults
+    this.state = this.defaults
   }
 
   componentDidMount () {
@@ -39,9 +38,9 @@ class App extends Component {
         <Main />
 
         <Footer
-          menu1={this.state.footer['menu_1']}
-          menu2={this.state.footer['menu_2']}
-          menu3={this.state.footer['menu_3']}
+          menu1={this.state.footer.menu1}
+          menu2={this.state.footer.menu2}
+          menu3={this.state.footer.menu3}
           socialMediaIcons={this.state.footer['social_media_icons']} />
 
       </div>
@@ -52,36 +51,46 @@ class App extends Component {
   * Here we add any network requests we want to be called on App init
   * - rememeber to add a default value to the state in the constructor
   * - the final top level App state will take this shape
+  *
+  * Network requests for menus are passed the slug of the registered menu location
+  * To use a menu here, it must first be registered in the theme with wp_register_nav_menu
   */
   async setInitialState () {
     try {
-      // TODO: split these requests so we set state after each one and don't have to wait for them all.
-      const primaryMenuRequest = NavMenuApiClient.getNavMenu('Primary')
-      const footerMenuRequest = NavMenuApiClient.getNavMenu('Footer Column 1')
-      const footerMenuRequest2 = NavMenuApiClient.getNavMenu('Footer Column 2')
-      const footerMenuRequest3 = NavMenuApiClient.getNavMenu('Footer Column 3')
-
+      const primaryMenuRequest = NavMenuApiClient.getNavMenu('primary')
       const primaryMenuResponse = await primaryMenuRequest
-      const footerResponse = await footerMenuRequest
-      const footerResponse2 = await footerMenuRequest2
-      const footerResponse3 = await footerMenuRequest3
-
-      // once we have the complete footer endpoint we can remove this footer object
-      // and put footerResponse.data in Object.assign
-      const footer = {
-        menu_1: footerResponse.data,
-        menu_2: footerResponse2.data,
-        menu_3: footerResponse3.data
-      }
-
-      this.setState({
-        primaryMenu: primaryMenuResponse.data,
-        footer: Object.assign({}, this.defaults.footer, footer)
-      })
+      this.setState({ primaryMenu: primaryMenuResponse.data })
     } catch (err) {
       console.log(new AppInitException(err))
-      return { ...this.state }
     }
+
+    const footer = this.defaults.footer
+
+    try {
+      const footerMenuRequest = NavMenuApiClient.getNavMenu('footer_col_1')
+      const footerResponse = await footerMenuRequest
+      footer.menu1 = footerResponse.data || []
+    } catch (err) {
+      console.log(new AppInitException(err))
+    }
+
+    try {
+      const footerMenuRequest2 = NavMenuApiClient.getNavMenu('footer_col_2')
+      const footerResponse2 = await footerMenuRequest2
+      footer.menu2 = footerResponse2.data || []
+    } catch (err) {
+      console.log(new AppInitException(err))
+    }
+
+    try {
+      const footerMenuRequest3 = NavMenuApiClient.getNavMenu('footer_col_3')
+      const footerResponse3 = await footerMenuRequest3
+      footer.menu3 = footerResponse3.data || []
+    } catch (err) {
+      console.log(new AppInitException(err))
+    }
+
+    this.setState({ footer })
   }
 }
 
