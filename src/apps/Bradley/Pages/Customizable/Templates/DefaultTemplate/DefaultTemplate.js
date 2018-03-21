625645dfd734080f03da0d81211e9705c075ba5e
Mario Vallejo <mario@vallgroup.com>
2018-03-21 09:10:01 -0500

    removed tabs. they were causing an issue when compiling.

--- src/apps/Bradley/Pages/Customizable/Templates/DefaultTemplate/DefaultTemplate.js
+++ src/apps/Bradley/Pages/Customizable/Templates/DefaultTemplate/DefaultTemplate.js
@@ -3,26 +3,25 @@ import PropTypes from 'prop-types'
 import style from './DefaultTemplate.scss'
 
 class DefaultTemplate extends Component {
+  renderTitle () {
+    return (
+      <div className={style.pageTitle}>
+        <h1
+          dangerouslySetInnerHTML={{
+            __html: this.props.data.page_title
+          }} />
+      </div>
+    )
+  }
 
-	renderTitle() {
-		return(
-			<div className={style.pageTitle}>
-	      <h1
-	      	dangerouslySetInnerHTML={{
-	      		__html: this.props.data.page_title
-	      	}} />
-	    </div>
-		)
-	}
-
-	render() {
-	  return (
-	    <div className={style.defaultTemplate}>
-	    	{this.renderTitle()}
-	      {this.props.renderModules()}
-	    </div>
-	  )
-	}
+  render () {
+    return (
+      <div className={style.defaultTemplate}>
+        {this.renderTitle()}
+        {this.props.renderModules()}
+      </div>
+    )
+  }
 }
 
 DefaultTemplate.propTypes = {
