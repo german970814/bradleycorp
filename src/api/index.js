e6926048d1015d5fc8537031438f3f4a16f60769
mattwills8 <matt_wills8@hotmail.co.uk>
2018-03-27 14:12:37 -0700

    add front end support for multisite with yarn commands to start each site

--- src/api/index.js
+++ src/api/index.js
@@ -2,10 +2,38 @@ import axios from 'axios'
 
 const api = {}
 
-// api.host = `http://bradley.test`
-api.host = `http://bradleydev.twoxfour.com`
+const dev = true
+
+api.host = (href => {
+  if (dev) {
+    if (href.includes('8081')) {
+      api.site = 'thewashfountain'
+      return `http://thewashfountain.bradley.test`
+    } else if (href.includes('8082')) {
+      api.site = 'bimrevit'
+      return `http://bimrevit.bradley.test`
+    } else {
+      api.site = 'bcorp'
+      return `http://bradley.test`
+    }
+  } else {
+    if (href.includes('thewashfountain.')) {
+      api.site = 'thewashfountain'
+      return `http://thewashfountain.bradleydev.twoxfour.com`
+    } else if (href.includes('bimrevit.')) {
+      api.site = 'bimrevit'
+      return `http://bimrevit.bradleydev.twoxfour.com`
+    } else {
+      api.site = 'bcorp'
+      return `http://bradleydev.twoxfour.com`
+    }
+  }
+})(location.href)
+
 api.namespace = `bcorp/v1/`
+
 api.baseURL = `${api.host}/index.php/wp-json/${api.namespace}`
+
 api.queryRoute = `wp-query`
 
 api.query = ({ args }) => {
