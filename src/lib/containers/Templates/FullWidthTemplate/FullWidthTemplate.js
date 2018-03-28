d14f7cd1e1e7a443f3fdae05c0e49b7f8d7a40fc
mattwills8 <matt_wills8@hotmail.co.uk>
2018-03-28 14:29:19 -0700

    separate templates from customizable page allowing us to use them with more flexibility

--- src/lib/containers/Templates/FullWidthTemplate/FullWidthTemplate.js
+++ src/lib/containers/Templates/FullWidthTemplate/FullWidthTemplate.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { validChain } from '../../../../../bcorpObject'
-import VideoBackground from '../../../../../components/BCorpVideo/VideoBackground/VideoBackground'
-import BCorpBackground from '../../../../../components/BCorpBackground/BCorpBackground'
+import { validChain } from '../../../bcorpObject'
+import VideoBackground from '../../../components/BCorpVideo/VideoBackground/VideoBackground'
+import BCorpBackground from '../../../components/BCorpBackground/BCorpBackground'
 import style from './FullWidthTemplate.scss'
 
 class FullWidthTemplate extends Component {
