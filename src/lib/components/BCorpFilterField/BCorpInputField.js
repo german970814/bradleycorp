593e5e07b96a0e6404973f4945ce677350580d80
mattwills8 <matt_wills8@hotmail.co.uk>
2018-04-23 09:41:23 -0500

    add standard input field to our BCorp filter fields

---
+++ src/lib/components/BCorpFilterField/BCorpInputField.js
@@ -0,0 +1,38 @@
+// @flow
+import * as React from 'react'
+import style from './BCorpFilterField.scss'
+
+type Props = {
+  filterState?: string,
+  handleChange: (event: SyntheticInputEvent<HTMLInputElement>) => void,
+  title?: string,
+  placeholder?: string,
+  className?: string
+}
+
+/**
+ * Class responsible for displaying and updating the input filter
+ */
+class BCorpInputField extends React.Component<Props> {
+  handleChange (event: SyntheticInputEvent<HTMLInputElement>) {
+    this.props.handleChange(event)
+  }
+
+  render () {
+    return (
+      <div className={`${this.props.className || ''} ${style.input}`}>
+        {this.props.title ? (
+          <h5 className={style.title}>{this.props.title}</h5>
+        ) : null}
+        <input
+          value={this.props.filterState}
+          onChange={this.handleChange.bind(this)}
+          placeholder={this.props.placeholder || ''}
+          type={'text'}
+        />
+      </div>
+    )
+  }
+}
+
+export default BCorpInputField
