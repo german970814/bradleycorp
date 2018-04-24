2e5f585dd68984003da65a74cdec60335bd36e6a
mattwills8 <matt_wills8@hotmail.co.uk>
2018-04-24 09:41:49 -0500

    add optional 'other' option to bcorp checkbox filter field

--- src/lib/components/BCorpFilterField/BCorpCheckboxField.js
+++ src/lib/components/BCorpFilterField/BCorpCheckboxField.js
@@ -1,26 +1,38 @@
 // @flow
 import * as React from 'react'
+import BCorpInputField from './BCorpInputField'
 import style from './BCorpFilterField.scss'
 
 type Options = {
   [string]: ?string
 }
 
+type CheckboxObject = {
+  checkboxes?: Array<string>,
+  other?: {
+    checked?: boolean,
+    content?: string
+  }
+}
+
 type Props = {
-  filterState?: Array<string>,
-  handleChange: (newCheckboxArray: Array<string>) => void,
+  filterState?: CheckboxObject,
+  handleChange: (newCheckboxObj: CheckboxObject) => void,
   options?: Options,
   title?: string,
   className?: string,
   checkboxOptionClassName?: string,
+  otherCheckboxClassName?: string,
   showOtherField?: boolean
 }
 
 class BCorpCheckboxField extends React.Component<Props> {
   handleChange (event: SyntheticInputEvent<HTMLInputElement>): void {
     let newCheckedState =
-      this.props.filterState && this.props.filterState.length
-        ? [...this.props.filterState]
+      this.props.filterState &&
+      this.props.filterState.checkboxes &&
+      this.props.filterState.checkboxes.length
+        ? [...this.props.filterState.checkboxes]
         : []
 
     const index = newCheckedState.findIndex(alreadyChecked => {
@@ -33,7 +45,63 @@ class BCorpCheckboxField extends React.Component<Props> {
       newCheckedState = [...newCheckedState, event.target.value]
     }
 
-    return this.props.handleChange(newCheckedState)
+    const newCheckboxObj = this.props.filterState || {}
+    newCheckboxObj.checkboxes = newCheckedState
+
+    return this.props.handleChange(newCheckboxObj)
+  }
+
+  handleOtherCheckboxChange (
+    event: SyntheticInputEvent<HTMLInputElement>
+  ): void {
+    const newCheckboxObj = this.props.filterState || {}
+    newCheckboxObj.other = newCheckboxObj.other || {}
+    newCheckboxObj.other.checked = !newCheckboxObj.other.checked
+    return this.props.handleChange(newCheckboxObj)
+  }
+
+  handleOtherInputChange (event: SyntheticInputEvent<HTMLInputElement>): void {
+    const newCheckboxObj = this.props.filterState || {}
+    newCheckboxObj.other = newCheckboxObj.other || {}
+    newCheckboxObj.other.content = event.target.value
+    return this.props.handleChange(newCheckboxObj)
+  }
+
+  renderOtherField () {
+    const inputState =
+      this.props.filterState &&
+      this.props.filterState.other &&
+      this.props.filterState.other.content
+        ? this.props.filterState.other.content
+        : ''
+
+    const colorState =
+      this.props.filterState &&
+      this.props.filterState.other &&
+      this.props.filterState.other.checked
+        ? ''
+        : style.fill
+
+    return this.props.showOtherField ? (
+      <div
+        className={`col1 ${this.props.otherCheckboxClassName || ''} ${
+          style.other
+        }`}>
+        <div className={`other-checkbox-option ${style.checkboxOption}`}>
+          <input
+            onChange={this.handleOtherCheckboxChange.bind(this)}
+            type="checkbox"
+            value={'checked'}
+          />
+          <label htmlFor={'checked'}>{'Other (please specify)'}</label>
+        </div>
+        <BCorpInputField
+          className={`${colorState}`}
+          filterState={inputState}
+          handleChange={this.handleOtherInputChange.bind(this)}
+        />
+      </div>
+    ) : null
   }
 
   renderOptions () {
@@ -70,9 +138,11 @@ class BCorpCheckboxField extends React.Component<Props> {
           </h5>
         ) : null}
         {this.renderOptions()}
+        {this.renderOtherField()}
       </div>
     )
   }
 }
 
 export default BCorpCheckboxField
+export type { CheckboxObject }
