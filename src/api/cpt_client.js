d6bb32ca46990f6628a26a8b8cb97b63c5e8560b
mattwills8 <matt_wills8@hotmail.co.uk>
2018-04-26 13:43:57 -0500

    add flow to api cpt client

--- src/api/cpt_client.js
+++ src/api/cpt_client.js
@@ -1,26 +1,39 @@
+// @flow
+import type { CPTName } from '../lib/types/cpt_types'
 import axios from 'axios'
 import api from './index'
 
 class CPTApiClient {
-  constructor (cptName) {
+  cptName: CPTName
+
+  constructor (cptName: CPTName) {
     this.cptName = cptName
   }
 
-  getById (id) {
+  /* GET posts */
+
+  getLatest (numberPosts: number = 0) {
+    const url = `${api.baseURL}${this.cptName}`
+    const params = { posts_per_page: numberPosts }
+
+    return axios.get(url, { params })
+  }
+
+  getById (id: number) {
     const url = `${api.baseURL}${this.cptName}`
     const params = { id }
 
     return axios.get(url, { params })
   }
 
-  getBySlug (slug) {
+  getBySlug (slug: string) {
     const url = `${api.baseURL}${this.cptName}`
     const params = { slug }
 
     return axios.get(url, { params })
   }
 
-  getByIdArray (idArray, postsPerPage = 10) {
+  getByIdArray (idArray: Array<number>, postsPerPage: number = 10) {
     const url = `${api.baseURL}${this.cptName}`
     const params = {
       id_array: JSON.stringify(idArray),
@@ -30,21 +43,14 @@ class CPTApiClient {
     return axios.get(url, { params })
   }
 
-  getLatest (numberPosts = 0) {
-    const url = `${api.baseURL}${this.cptName}`
-    const params = { posts_per_page: numberPosts }
-
-    return axios.get(url, { params })
-  }
-
-  getByTax (taxName, termSlug) {
+  getByTax (taxName: string, termSlug: string) {
     const url = `${api.baseURL}${this.cptName}`
     const params = { tax_name: taxName, term_slug: termSlug }
 
     return axios.get(url, { params })
   }
 
-  getByTaxAndTermArray (taxName, termSlugArray) {
+  getByTaxAndTermArray (taxName: string, termSlugArray: Array<string>) {
     const url = `${api.baseURL}${this.cptName}`
     const params = {
       tax_name: taxName,
@@ -54,19 +60,7 @@ class CPTApiClient {
     return axios.get(url, { params })
   }
 
-  getHeirarchyById (id) {
-    const url = `${api.baseURL}${this.cptName}-heirarchy`
-    const params = { id }
-
-    return axios.get(url, { params })
-  }
-
-  getTreeById (id) {
-    const url = `${api.baseURL}${this.cptName}-tree`
-    const params = { id }
-
-    return axios.get(url, { params })
-  }
+  /* GET Terms */
 
   getTerms () {
     const url = `${api.baseURL}${this.cptName}-terms`
@@ -74,14 +68,32 @@ class CPTApiClient {
     return axios.get(url)
   }
 
-  getTermsByTax (taxName) {
+  getTermsByTax (taxName: string) {
     const url = `${api.baseURL}${this.cptName}-terms`
     const params = { 'tax-name': taxName }
 
     return axios.get(url, { params })
   }
 
-  get (page = 1) {
+  /* GET Child/Parent Trees */
+
+  getHeirarchyById (id: number) {
+    const url = `${api.baseURL}${this.cptName}-heirarchy`
+    const params = { id }
+
+    return axios.get(url, { params })
+  }
+
+  getTreeById (id: number) {
+    const url = `${api.baseURL}${this.cptName}-tree`
+    const params = { id }
+
+    return axios.get(url, { params })
+  }
+
+  /* Extras //TODO: remove */
+
+  get (page: number = 1) {
     const args = {
       post_type: this.cptName,
       paged: page
--- src/lib/types/cpt_types.js
+++ src/lib/types/cpt_types.js
@@ -1,9 +1,22 @@
 import type { WPPost } from './post_types'
 
+type CPTName =
+  | 'application-gallery'
+  | 'case-study'
+  | 'chip'
+  | 'faq'
+  | 'literature'
+  | 'news'
+  | 'product'
+  | 'technical-info'
+  | 'video-gallery'
+  | 'warranty'
+  | 'nav_menu_item'
+
 type LiteraturePost = WPPost & { post_type: 'literature' }
 
 type ChipSamplePost = WPPost & { post_type: 'chip_sample' }
 
 type NavMenuItem = WPPost & { post_type: 'nav_menu_item' }
 
-export type { LiteraturePost, ChipSamplePost, NavMenuItem }
+export type { CPTName, LiteraturePost, ChipSamplePost, NavMenuItem }
