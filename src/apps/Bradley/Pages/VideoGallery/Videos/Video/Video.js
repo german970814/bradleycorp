5509936d5d5382d30cf05c1e883fee33c1fc299d
mattwills8 <matt_wills8@hotmail.co.uk>
2018-04-27 17:17:15 -0500

    add styling to videos and support for keeping player time in sync between video inside and outside lightbox

--- src/apps/Bradley/Pages/VideoGallery/Videos/Video/Video.js
+++ src/apps/Bradley/Pages/VideoGallery/Videos/Video/Video.js
@@ -1,6 +1,7 @@
 // @flow
 import * as React from 'react'
 import type { VideoGalleryPost } from '../../../../../../lib/types/cpt_types'
+import LightboxV2 from '../../../../../../lib/containers/Lightbox/LightboxV2/LightboxV2'
 import BCorpVideo from '../../../../../../lib/components/BCorpVideo/BCorpVideo'
 import FixedAspectRatioBox from '../../../../../../lib/components/FixedAspectRatioBox/FixedAspectRatioBox'
 import style from './Video.scss'
@@ -9,17 +10,121 @@ type Props = {
   video: VideoGalleryPost
 }
 
-const Video = (props: Props) => {
-  const url = props.video.meta.video_gallery_video
+type State = {
+  time: number
+}
 
-  return (
-    <div className={`col1 col2-tablet ${style.videoWrapper}`}>
-      <FixedAspectRatioBox>
-        <BCorpVideo className={style.video} url={url} noVimeo />
-      </FixedAspectRatioBox>
-      <h5 className={style.title}>{props.video.post.post_title}</h5>
-    </div>
-  )
+type YouTubeAPI = {
+  playVideo: () => void,
+  pauseVideo: () => void,
+  getCurrentTime: () => number,
+  seekTo: (time: number) => void,
+  ready: boolean
+}
+
+class Video extends React.Component<Props, State> {
+  url: string
+  opts: {}
+  youtubeAPIClosed: ?YouTubeAPI
+  youtubeAPIOpen: ?YouTubeAPI
+
+  constructor (props: Props) {
+    super(props)
+
+    this.state = {
+      time: 0
+    }
+
+    this.url = this.props.video.meta.video_gallery_video
+    this.opts = {
+      playerVars: {
+        modestbranding: 1,
+        showinfo: 0
+      }
+    }
+  }
+
+  render () {
+    return (
+      <LightboxV2
+        renderChildren={openLightbox => {
+          return (
+            <div className={`col1 col2-tablet ${style.videoWrapper}`}>
+              <FixedAspectRatioBox>
+                <div className={style.videoOverlayWrapper}>
+                  <BCorpVideo
+                    className={style.video}
+                    url={this.url}
+                    youtubeProps={{
+                      onReady: event => {
+                        this.youtubeAPIClosed = event.target
+                        this.youtubeAPIClosed.ready = true
+                      },
+                      onPlay: event => {
+                        if (this.youtubeAPIClosed) {
+                          this.youtubeAPIClosed.pauseVideo()
+                          openLightbox()
+                        }
+                      },
+                      opts: this.opts
+                    }}
+                    noVimeo
+                  />
+                  <img
+                    onClick={event => {
+                      if (
+                        this.youtubeAPIClosed &&
+                        this.youtubeAPIClosed.ready
+                      ) {
+                        this.youtubeAPIClosed.playVideo()
+                      }
+                    }}
+                    className={style.playButton}
+                    src={require('../../../../../../images/play-button/play@2x.png')}
+                  />
+                </div>
+              </FixedAspectRatioBox>
+              <h5 className={style.title}>
+                {this.props.video.post.post_title}
+              </h5>
+            </div>
+          )
+        }}
+        renderLightboxContents={() => {
+          return (
+            <FixedAspectRatioBox>
+              <BCorpVideo
+                className={style.video}
+                url={this.url}
+                youtubeProps={{
+                  opts: this.opts,
+                  onReady: event => {
+                    if (this.youtubeAPIOpen) {
+                      this.youtubeAPIOpen = event.target
+                      this.youtubeAPIOpen.seekTo(this.state.time)
+                    }
+                  }
+                }}
+                noVimeo
+                autoplay
+              />
+            </FixedAspectRatioBox>
+          )
+        }}
+        onLightboxClose={() => {
+          if (this.youtubeAPIOpen && this.youtubeAPIClosed) {
+            const youtubeAPIClosed = this.youtubeAPIClosed
+            const time = this.youtubeAPIOpen.getCurrentTime()
+            youtubeAPIClosed.seekTo(time)
+            this.setState({ time })
+          }
+        }}
+        fitLightboxToContent
+        fullWidth
+        maxWidth={'792px'}
+      />
+    )
+  }
 }
 
 export default Video
