dd8f20aec463f6223c2fd7c55081456e1d12deba
mattwills8 <matt_wills8@hotmail.co.uk>
2018-04-27 09:55:24 -0500

    add 1500ms debounce to video request and loading state from first filter changed to post response arrives

--- src/apps/Bradley/Pages/VideoGallery/Videos/Videos.js
+++ src/apps/Bradley/Pages/VideoGallery/Videos/Videos.js
@@ -1,11 +1,13 @@
 // @flow
 import * as React from 'react'
+import debounce from 'debounce'
 import type { FiltersType } from '../VideoGallery'
 import type { VideoGalleryPost } from '../../../../../lib/types/cpt_types'
 import type { BCorpPost } from '../../../../../lib/types/post_types'
 import { vimeoParser } from '../../../../../lib/components/BCorpVideo/BCorpVideo'
 import { filterDefault } from '../VideoGallery'
 import { sortIntoRows } from '../../../../../lib/bcorpJSX'
+import Loading from '../../../../../lib/components/Loading/Loading'
 import Video from './Video/Video'
 import CPTApiClient from '../../../../../api/cpt_client'
 import style from './Videos.scss'
@@ -19,14 +21,19 @@ type VideoType = BCorpPost & { post: VideoGalleryPost }
 type VideoStateType = Array<VideoType>
 
 type State = {
-  videos?: VideoStateType
+  videos?: VideoStateType,
+  loading: boolean
 }
 
 class Videos extends React.Component<Props, State> {
+  getFilteredVideosDebounced: (props: Props) => void
+
   constructor (props: Props) {
     super(props)
 
-    this.state = {}
+    this.state = { loading: true }
+
+    this.getFilteredVideosDebounced = debounce(this.getFilteredVideos, 1500)
   }
 
   componentDidMount () {
@@ -35,7 +42,8 @@ class Videos extends React.Component<Props, State> {
 
   componentWillReceiveProps (nextProps: Props) {
     if (this.shouldResendRequest(nextProps)) {
-      this.getFilteredVideos(nextProps)
+      this.setState({ loading: true })
+      this.getFilteredVideosDebounced(nextProps)
     }
   }
 
@@ -61,18 +69,23 @@ class Videos extends React.Component<Props, State> {
 
   render () {
     console.log(this.state)
-    return <div className={`row ${style.videos}`}>{this.renderVideos()}</div>
+    return (
+      <div className={`row ${style.videos}`}>
+        {this.state.loading ? <Loading /> : this.renderVideos()}
+      </div>
+    )
   }
 
   async getFilteredVideos (props: Props) {
     const filters = this.getFiltersFormattedForRequest(props)
 
     try {
+      console.log('sending', filters)
       const client = new CPTApiClient('video-gallery')
       const response = await client.getByTaxNameAndTermSlugObject(filters, 'OR')
 
       const videos: Array<VideoType> = response.data
-      return this.setState({ videos })
+      return this.setState({ videos, loading: false })
     } catch (err) {
       console.log(err)
     }
