6d14b0c1f0fda97182a710f32b93c258c68cdf45
mattwills8 <matt_wills8@hotmail.co.uk>
2018-05-01 18:25:01 -0500

    make blog landing page into reusable template (incl widgets, title, layout) which takes content as a render props.

--- src/lib/containers/Pages/BlogLandingPage/BlogLandingPage.js
+++ src/lib/containers/Pages/BlogLandingPage/BlogLandingPage.js
@@ -3,29 +3,23 @@ import React, { Component } from 'react'
 import type { SiteType } from '../../../../api'
 import type { Match } from 'react-router-dom'
 import type { BCorpPost } from '../../../types/post_types'
-import type { Widget } from '../../../types/widget_types'
 import { validChain } from '../../../bcorpObject'
-import { site, sitePrettyName } from '../../../../api'
+import { site } from '../../../../api'
 import BIMRevitClient from '../../../../api/bimRevit_client'
 import TheWashfountainClient from '../../../../api/theWashfountain_client'
-import WidgetsClient from '../../../../api/widgets_client'
-import WidgetBuilder from '../../Widgets/WidgetBuilder'
-import RightSidebarTemplate from '../../Templates/RightSidebarTemplate/RightSidebarTemplate'
+import BlogPageTemplate from '../../Templates/BlogPageTemplate/BlogPageTemplate'
 import Posts from './Posts/Posts'
-import style from './BlogLandingPage.scss'
 
 type Props = {
   match: Match
 }
 
 type State = {
-  posts: Array<BCorpPost>,
-  widgets: Array<Widget>
+  posts: Array<BCorpPost>
 }
 
 class BlogLandingPage extends Component<Props, State> {
   defaultPostState: BCorpPost
-  defaultWidgetState: Widget
   defaultState: State
 
   constructor (props: Props) {
@@ -51,16 +45,8 @@ class BlogLandingPage extends Component<Props, State> {
       }
     }
 
-    this.defaultWidgetState = {
-      type: '',
-      data: {
-        title: 'Loading Widget...'
-      }
-    }
-
     this.defaultState = {
-      posts: [this.defaultPostState],
-      widgets: [this.defaultWidgetState]
+      posts: [this.defaultPostState]
     }
 
     this.state = this.defaultState
@@ -68,14 +54,10 @@ class BlogLandingPage extends Component<Props, State> {
 
   componentDidMount () {
     this.getPosts()
-    this.getWidgets()
   }
 
   /**
    * If we go between urls that both render a blog landing page, we need to make sure we're making a new network request
-   *
-   * For the moment we can be sure that we don't need to re request the widgets,
-   * if we end up with multiple sidebars then that could change
    *
    * @param  {[object]} nextProps
    * @return {[void]}
@@ -95,25 +77,13 @@ class BlogLandingPage extends Component<Props, State> {
 
   render () {
     return (
-      <div className={style.blogLandingPage}>
-        <RightSidebarTemplate
-          data={{
-            page_title: sitePrettyName
-          }}
-          renderModules={() => {
+      <div className={`Blog-Landing-Page`}>
+        <BlogPageTemplate
+          renderContent={() => {
             return (
               <div className={'row'}>
                 <Posts data={this.state.posts} />
               </div>
-            )
-          }}
-          renderRightSidebarWidgets={() => {
-            return (
-              <WidgetBuilder
-                widgetData={this.state.widgets}
-                pagePath={this.props.match.url}
-                twoColsOnTablet
-              />
             )
           }}
         />
@@ -139,32 +109,13 @@ class BlogLandingPage extends Component<Props, State> {
 
       // passing 0 means we use WP get_posts default number of posts (currently 5)
       const response = await client.getRecentPosts(0)
-      const postsData = response.data
+      const postsData: Array<BCorpPost> = response.data
 
       const posts = postsData.map(postData => {
         return Object.assign({}, this.defaultPostState, postData)
       })
 
       return this.setState({ posts })
-    } catch (err) {
-      console.log(err)
-    }
-  }
-
-  /**
-   * Gets the widgets and merges them with state,
-   * keeping any required defaults that aren't included in the data
-   */
-  async getWidgets () {
-    try {
-      const response = await WidgetsClient.getBlogSidebar()
-      const widgetsData: Array<Widget> = response.data
-
-      const widgets = widgetsData.map(widgetData => {
-        return Object.assign({}, this.defaultWidgetState, widgetData)
-      })
-
-      return this.setState({ widgets })
     } catch (err) {
       console.log(err)
     }
