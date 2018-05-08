b0e11261feaf1d616948d8ea54860539340622bb
mattwills8 <matt_wills8@hotmail.co.uk>
2018-05-08 10:39:19 -0500

    fix little linting error

--- src/apps/Bradley/Pages/ApplicationGallery/Filters/ProductCheck.js
+++ src/apps/Bradley/Pages/ApplicationGallery/Filters/ProductCheck.js
@@ -5,7 +5,7 @@ import type { Options } from './Filters'
 import type { CheckboxObject } from '../../../../../lib/components/BCorpFilterField/BCorpCheckboxField'
 import type { FiltersType } from '../ApplicationGallery'
 import Collapsible from 'react-collapsible'
-import { MOBILEMAXWIDTH, TABLETMAXWIDTH } from '../../../../../globals'
+import { MOBILEMAXWIDTH } from '../../../../../globals'
 import Media from 'react-media'
 import style from './Filters.scss'
 
@@ -38,34 +38,51 @@ class ProductCheck extends React.Component<Props, State> {
   }
 
   renderCheckbox () {
-    return <BCorpCheckboxField
-      title={this.props.title.replace(/_/g, ' ')}
-      filterState={{ checkboxes: this.state.checkboxes }}
-      handleChange={this.handleChange.bind(this)}
-      options={this.props.options}
-    />
+    return (
+      <BCorpCheckboxField
+        title={this.props.title.replace(/_/g, ' ')}
+        filterState={{ checkboxes: this.state.checkboxes }}
+        handleChange={this.handleChange.bind(this)}
+        options={this.props.options}
+      />
+    )
   }
 
   renderCheckboxMobile () {
-    return <BCorpCheckboxField
-      filterState={{ checkboxes: this.state.checkboxes }}
-      handleChange={this.handleChange.bind(this)}
-      options={this.props.options}
-    />
+    return (
+      <BCorpCheckboxField
+        filterState={{ checkboxes: this.state.checkboxes }}
+        handleChange={this.handleChange.bind(this)}
+        options={this.props.options}
+      />
+    )
   }
 
   render () {
-    return <Media query={{ maxWidth: MOBILEMAXWIDTH }}>
-      {match =>
-        match ? (
-          // mobile
-          <Collapsible trigger={this.props.title.replace(/_/g, ' ')} triggerTagName={'button'} classParentString={`Collapsible ${style.checkBoxTitle}`} triggerStyle={{ color: '#a7a9ac', fontSize: '13px', fontWeight: '300', textTransform: 'uppercase', letterSpacing: '0.5px' }}>
-            { this.renderCheckboxMobile() }
-          </Collapsible>
-        ) : this.renderCheckbox()
-      }
-    </Media>
-    
+    return (
+      <Media query={{ maxWidth: MOBILEMAXWIDTH }}>
+        {match =>
+          match ? (
+            // mobile
+            <Collapsible
+              trigger={this.props.title.replace(/_/g, ' ')}
+              triggerTagName={'button'}
+              classParentString={`Collapsible ${style.checkBoxTitle}`}
+              triggerStyle={{
+                color: '#a7a9ac',
+                fontSize: '13px',
+                fontWeight: '300',
+                textTransform: 'uppercase',
+                letterSpacing: '0.5px'
+              }}>
+              {this.renderCheckboxMobile()}
+            </Collapsible>
+          ) : (
+            this.renderCheckbox()
+          )
+        }
+      </Media>
+    )
   }
 }
 
