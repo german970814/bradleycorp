dc68377b93a23d50a242e32decedff4cdb7abb39
German <german.alzate970814@gmail.com>
2018-05-16 18:01:52 -0500

    Product page with data

--- src/apps/Bradley/Pages/Results/ContentTabs/Products.js
+++ src/apps/Bradley/Pages/Results/ContentTabs/Products.js
@@ -5,6 +5,7 @@ import { MOBILEMAXWIDTH, TABLETMAXWIDTH } from '../../../../../globals'
 import FillColumns from '../../../../../lib/components/FillColumns/FillColumns'
 import style from './../Results.scss'
 import Default from './Default'
+import type { ProductPost } from '../../../../../lib/types/cpt_types'
 
 export default class SearchProducts extends Default {
 
@@ -12,16 +13,23 @@ export default class SearchProducts extends Default {
     return this.props.posts && this.props.posts.map((post, ind) => {
       return <article key={ind}>
         <div className={`${style.newH}`}>
-          <span className={`${style.new}`}>NEW</span>
+          {this.isProductNew(post) && <span className={`${style.new}`}>NEW</span>}
           <span className={`${style.h}`}>H</span>
         </div>
         <div className={`${style.productsImageContainer}`}>
-          <img src={require('../../../../../images/results/products-mobile.jpg')}/>
+          {post.meta.product_media.images.length && 
+            <img src={Array.isArray(post.meta.product_media.images) ? post.meta.product_media.images[0] : post.meta.product_media.images} />}
         </div>
-        <h6 className={`${style.productReference}`}>MODEL S19-304</h6>
-        <p className={`${style.productDescription}`}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        <h6 className={`${style.productReference}`}>MODEL {post.meta.product_sku}</h6>
+        <p className={`${style.productDescription}`}>{post.post.post_excerpt}</p>
       </article>
     })
+  }
+
+  isProductNew(product: ProductPost) {
+    const dateUntilNew = new Date(product.meta.product_new_until)
+    const today = new Date()
+    return today < dateUntilNew
   }
 
   renderColumns (classes: string) {
