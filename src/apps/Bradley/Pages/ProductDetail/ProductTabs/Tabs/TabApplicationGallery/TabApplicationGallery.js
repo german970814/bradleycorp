e125a2352f1c558ec226a64f2e02e52f4c76231c
mattwills8 <matt_wills8@hotmail.co.uk>
2018-05-21 15:41:10 -0500

    add application gallery tab to product tabs

--- src/apps/Bradley/Pages/ProductDetail/ProductTabs/Tabs/TabApplicationGallery/TabApplicationGallery.js
+++ src/apps/Bradley/Pages/ProductDetail/ProductTabs/Tabs/TabApplicationGallery/TabApplicationGallery.js
@@ -1,35 +1,39 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+// @flow
+import * as React from 'react'
+import type { BCorpPost } from '../../../../../../../lib/types/post_types'
+import { Link } from 'react-router-dom'
+import { DarkGray } from '../../../../../../../scss/partials/_variables.scss'
 import tabStyle from '../Tabs.scss'
 import style from './TabApplicationGallery.scss'
 
-class TabApplicationGallery extends Component {
-  renderApplicationGallery () {
-    return this.props.applicationGalleries.map((applicationGallery, index) => {
-      return (
-        <li
-          key={index} >
-          {applicationGallery.post['post_title']}
-        </li>
-      )
-    })
-  }
+type Props = {
+  applicationGalleries: Array<BCorpPost>
+}
 
+class TabApplicationGallery extends React.Component<Props> {
   render () {
     return (
-      <div
-        className={[style.tabApplicationGallery, tabStyle.fullWidthColDesktopTab].join(' ')} >
-        <ul
-          className={tabStyle.tabColUl} >
-          {this.renderApplicationGallery()}
-        </ul>
+      <div className={style.tabApplicationGallery}>
+        {this.props.applicationGalleries.map((post, index) => {
+          const inlineStyle = post.meta.app_gallery_img
+            ? {
+              backgroundImage: `url('${post.meta.app_gallery_img}')`
+            }
+            : {
+              backgroundColor: DarkGray
+            }
+
+          return (
+            <Link
+              key={index}
+              to={`/application-gallery/${post.post.post_name || ''}`}>
+              <div style={inlineStyle} className={style.image} />
+            </Link>
+          )
+        })}
       </div>
     )
   }
-}
-
-TabApplicationGallery.propTypes = {
-  applicationGalleries: PropTypes.array.isRequired
 }
 
 export default TabApplicationGallery
