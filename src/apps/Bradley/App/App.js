c7329b05490b0c975a7225e1aab60536b793aa90
mattwills8 <matt_wills8@hotmail.co.uk>
2018-05-22 11:16:46 -0500

    add screen size context - still need to remove other Media instances

--- src/apps/Bradley/App/App.js
+++ src/apps/Bradley/App/App.js
@@ -3,9 +3,13 @@ import * as React from 'react'
 import type { SiteType } from '../../../api'
 import type { User } from '../../../lib/types/user_types'
 import type { UpdateUserType } from '../../../lib/contexts/UserContext'
+import type { ScreenSize } from '../../../lib/contexts/ScreenSizeContext'
+import Media from 'react-media'
 import { CookiesProvider } from 'react-cookie'
 import { UserProvider } from '../../../lib/contexts/UserContext'
+import { ScreenSizeProvider } from '../../../lib/contexts/ScreenSizeContext'
 import { site } from '../../../api'
+import { MOBILEMAXWIDTH, TABLETMAXWIDTH } from '../../../globals'
 import Main from '../Main/Main'
 import MainBIMRevit from '../BIMRevit/Main/Main'
 import MainTheWashfountain from '../TheWashfountain/Main/Main'
@@ -55,17 +59,43 @@ class App extends React.Component<Props, State> {
     }
   }
 
+  getScreenSize (isMobile: boolean, isTablet: boolean): ScreenSize {
+    let screenSize = 'desktop'
+    if (isMobile) {
+      screenSize = 'mobile'
+    } else if (isTablet) {
+      screenSize = 'tablet'
+    }
+
+    return screenSize
+  }
+
   render () {
     const { user, updateUser } = this.state
 
     return (
       <UserProvider value={{ user, updateUser }}>
         <CookiesProvider>
-          <div id={'app'} className={`${style.app}`}>
-            {this.getMain()}
+          <Media query={{ maxWidth: MOBILEMAXWIDTH }}>
+            {isMobile => {
+              return (
+                <Media query={{ maxWidth: TABLETMAXWIDTH }}>
+                  {isTablet => {
+                    const screenSize = this.getScreenSize(isMobile, isTablet)
+                    return (
+                      <ScreenSizeProvider value={{ screenSize }}>
+                        <div id={'app'} className={`${style.app}`}>
+                          {this.getMain()}
 
-            <Footer />
-          </div>
+                          <Footer />
+                        </div>
+                      </ScreenSizeProvider>
+                    )
+                  }}
+                </Media>
+              )
+            }}
+          </Media>
         </CookiesProvider>
       </UserProvider>
     )
