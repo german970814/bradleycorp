774f3772db32867d9b0ef65449c4f15ad625261b
Mario Vallejo <mario@vallgroup.com>
2018-05-31 10:04:14 -0500

    Loading is now a class that takes `center` as a prop which will center the loading icon within the component. It also takes style as an additional prop to further style the component

--- src/lib/components/Loading/Loading.js
+++ src/lib/components/Loading/Loading.js
@@ -4,21 +4,36 @@ import styles from './Loading.scss'
 /**
  * Will be created in a future sprint with designs
  */
-const Loading = props => {
-  return <div className={styles.skFadingCircle}>
-	  <div className={styles.skCircle}></div>
-	  <div className={[styles.skCircle2, styles.skCircle].join(' ')}></div>
-	  <div className={[styles.skCircle3, styles.skCircle].join(' ')}></div>
-	  <div className={[styles.skCircle4, styles.skCircle].join(' ')}></div>
-	  <div className={[styles.skCircle5, styles.skCircle].join(' ')}></div>
-	  <div className={[styles.skCircle6, styles.skCircle].join(' ')}></div>
-	  <div className={[styles.skCircle7, styles.skCircle].join(' ')}></div>
-	  <div className={[styles.skCircle8, styles.skCircle].join(' ')}></div>
-	  <div className={[styles.skCircle9, styles.skCircle].join(' ')}></div>
-	  <div className={[styles.skCircle10, styles.skCircle].join(' ')}></div>
-	  <div className={[styles.skCircle11, styles.skCircle].join(' ')}></div>
-	  <div className={[styles.skCircle12, styles.skCircle].join(' ')}></div>
-	</div>
+class Loading extends React.Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {}
+	}
+
+	render() {
+
+		let __styles = {}
+
+		if ( this.props.center ) {
+			__styles = {...__styles, display: 'block', margin: '30px auto'}
+		}
+
+		return <div className={styles.skFadingCircle} style={{...__styles, ...this.props.style}}>
+		  <div className={styles.skCircle}></div>
+		  <div className={[styles.skCircle2, styles.skCircle].join(' ')}></div>
+		  <div className={[styles.skCircle3, styles.skCircle].join(' ')}></div>
+		  <div className={[styles.skCircle4, styles.skCircle].join(' ')}></div>
+		  <div className={[styles.skCircle5, styles.skCircle].join(' ')}></div>
+		  <div className={[styles.skCircle6, styles.skCircle].join(' ')}></div>
+		  <div className={[styles.skCircle7, styles.skCircle].join(' ')}></div>
+		  <div className={[styles.skCircle8, styles.skCircle].join(' ')}></div>
+		  <div className={[styles.skCircle9, styles.skCircle].join(' ')}></div>
+		  <div className={[styles.skCircle10, styles.skCircle].join(' ')}></div>
+		  <div className={[styles.skCircle11, styles.skCircle].join(' ')}></div>
+		  <div className={[styles.skCircle12, styles.skCircle].join(' ')}></div>
+		</div>
+	}
 }
 
 export default Loading
