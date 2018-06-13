41e00c714bf0f96209746d4a14cef35563457a43
mattwills8 <matt_wills8@hotmail.co.uk>
2018-06-12 20:22:06 -0500

    add without thumbnail desktop mega menu

---
+++ src/lib/containers/MegaMenu/MegaMenuItemsHover/MegaMenu/MegaMenuExpanded/WithoutThumbnails/MenuBlock/MenuBlock.js
@@ -0,0 +1,68 @@
+// @flow
+import * as React from 'react'
+import type { MegaMenuNavMenuItem } from '../../../../../../../types/megaMenu_types'
+import { Link } from 'react-router-dom'
+import BCorpLink from '../../../../../../../components/BCorpLink/BCorpLink'
+import style from './MenuBlock.scss'
+
+type Props = {
+  menuItem: MegaMenuNavMenuItem
+}
+
+class MenuBlock extends React.PureComponent<Props> {
+  renderChildItems () {
+    return this.props.menuItem.children.map((child, index) => {
+      return (
+        <BCorpLink
+          url={child.url}
+          renderInternal={url => {
+            return (
+              <Link to={url}>
+                <div key={index} className={`small-body ${style.childItem}`}>
+                  {child.title}
+                </div>
+              </Link>
+            )
+          }}
+          renderExternal={url => {
+            return (
+              <a href={url} target="_blank">
+                <div key={index} className={`small-body ${style.childItem}`}>
+                  {child.title}
+                </div>
+              </a>
+            )
+          }}
+        />
+      )
+    })
+  }
+
+  render () {
+    return (
+      <div className={style.menuBlock}>
+        <BCorpLink
+          url={this.props.menuItem.url}
+          renderInternal={url => {
+            return (
+              <Link to={url}>
+                <h6 className={style.title}>{this.props.menuItem.title}</h6>
+                {this.renderChildItems()}
+              </Link>
+            )
+          }}
+          renderExternal={url => {
+            return (
+              <a href={url} target="_blank">
+                <h6 className={style.title}>{this.props.menuItem.title}</h6>
+                {this.renderChildItems()}
+              </a>
+            )
+          }}
+        />
+      </div>
+    )
+  }
+}
+
+export default MenuBlock
