ad3e068d7a0449892f72899f3a4bad649689957f
mattwills8 <matt_wills8@hotmail.co.uk>
2018-06-13 11:16:01 -0500

    make sure nav menu items have the correct link

--- src/lib/types/megaMenu_types.js
+++ src/lib/types/megaMenu_types.js
@@ -35,6 +35,8 @@ type NavMenuItemPost = {
   title: string,
   url: string,
   object_featured_image: string | false,
+  object_post_type?: string,
+  object_post_name?: string,
   children: Array<NavMenuItemPost>
 }
 
