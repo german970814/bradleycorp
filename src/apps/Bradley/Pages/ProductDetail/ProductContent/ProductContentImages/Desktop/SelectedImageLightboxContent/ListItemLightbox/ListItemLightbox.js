ccffd58bedf140cc8d6b94179af260c07245a0cc
mattwills8 <matt_wills8@hotmail.co.uk>
2018-06-15 19:09:21 -0500

    make sure all video instances use BCorpVideo

--- src/apps/Bradley/Pages/ProductDetail/ProductContent/ProductContentImages/Desktop/SelectedImageLightboxContent/ListItemLightbox/ListItemLightbox.js
+++ src/apps/Bradley/Pages/ProductDetail/ProductContent/ProductContentImages/Desktop/SelectedImageLightboxContent/ListItemLightbox/ListItemLightbox.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import YouTube from 'react-youtube'
+import BCorpVideo from '../../../../../../../../../lib/components/BCorpVideo/BCorpVideo'
 import { youtubeParser } from '../../../../../../../../../lib/bcorpUrl'
 import LIGHTBOXSIZES from '../../../../../../../../../lib/containers/Lightbox/lightboxVars'
 import FixedAspectRatioBox from '../../../../../../../../../lib/components/FixedAspectRatioBox/FixedAspectRatioBox'
@@ -12,24 +12,26 @@ class ListItemLightbox extends Component {
       backgroundImage: `url(${this.props.src})`
     }
 
-    return (
-      <div
-        style={imageStyle}
-        className={style.listItemLightbox} />
-    )
+    return <div style={imageStyle} className={style.listItemLightbox} />
   }
 
   renderVideo () {
     const videoId = youtubeParser(this.props.src) || ''
     return (
-      <FixedAspectRatioBox
-        maxHeight={LIGHTBOXSIZES.heightMinusCloseButton} >
-        <YouTube
-          videoId={videoId}
-          opts={{
+      <FixedAspectRatioBox maxHeight={LIGHTBOXSIZES.heightMinusCloseButton}>
+        <BCorpVideo
+          url={this.props.src}
+          youtubeProps={{
+            opts: {
+              width: '100%',
+              height: '100%'
+            }
+          }}
+          vimeoProps={{
             width: '100%',
             height: '100%'
-          }} />
+          }}
+        />
       </FixedAspectRatioBox>
     )
   }
