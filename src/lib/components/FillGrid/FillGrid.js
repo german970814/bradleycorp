cd5261a2d914a1d624b1d2b65a80705a573952fa
mattwills8 <matt_wills8@hotmail.co.uk>
2018-07-05 14:05:38 +0200

    add a grid component to auto sort elements into rows and columns

---
+++ src/lib/components/FillGrid/FillGrid.js
@@ -0,0 +1,38 @@
+// @flow
+import * as React from 'react'
+import { sortIntoRows } from '../../bcorpJSX'
+
+type Props = {
+  children: React.ChildrenArray<React.Element<any>>,
+  colClasses: Array<string>
+}
+
+/**
+ * Given a number of child elements,
+ * this component renders them into a grid with columns of user defined width.
+ *
+ * We loop through the given element applying a column class from the array,
+ * when we reach the end of the col class array we start a new row
+ */
+class FillGrid extends React.Component<Props> {
+  renderColumns () {
+    const { colClasses } = this.props
+
+    return React.Children.map(this.props.children, (child, index) => {
+      // makes sure we loop through the colClasses if length of children if bigger
+      const colClass = colClasses[index % colClasses.length]
+
+      return (
+        <div key={index} className={colClass}>
+          {child}
+        </div>
+      )
+    })
+  }
+
+  render () {
+    return sortIntoRows(this.renderColumns(), this.props.colClasses.length)
+  }
+}
+
+export default FillGrid
