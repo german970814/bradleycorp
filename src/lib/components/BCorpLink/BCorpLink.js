3cef7fb246d82c46ece0d589db911f043dbba7ec
mattwills8 <matt_wills8@hotmail.co.uk>
2018-07-09 12:37:24 +0200

    add flow to bcorp link

--- src/lib/components/BCorpLink/BCorpLink.js
+++ src/lib/components/BCorpLink/BCorpLink.js
@@ -1,21 +1,29 @@
-import { Component } from 'react'
-import PropTypes from 'prop-types'
+// @flow
+import * as React from 'react'
 import { removeHostFromUrl } from '../../bcorpUrl'
 
 /**
  * Wrapper for react router Link components.
- * Allows us to check for an external link being passed and render a different component for each case
+ * Allows us to check for an external link being passed
+ * and render a different component for each case
  *
- * eg we might pass an <a> tag to renderExternal and a <Link> tag to renderInternal
+ * eg we might pass an <a> tag to renderExternal
+ * and a <Link> tag to renderInternal
  */
-class BCorpLink extends Component {
+
+type Props = {
+  url: string,
+  renderInternal: (url: string) => React.Node,
+  renderExternal: (url: string) => React.Node
+}
+
+class BCorpLink extends React.Component<Props> {
   /**
    * Checks if the link is internal
    *
-   * If so it calls the renderInternal function prop passing it the react router friendly link with host removed,
+   * If so it calls the renderInternal function prop
+   * passing it the react router friendly link with host removed,
    * otherwise it calls renderExternal and passes the full url
-   *
-   * @return {[void]} Calls render prop functions
    */
   render () {
     const internal = removeHostFromUrl(this.props.url)
@@ -24,12 +32,6 @@ class BCorpLink extends Component {
       ? this.props.renderInternal(internal)
       : this.props.renderExternal(this.props.url)
   }
-}
-
-BCorpLink.propTypes = {
-  url: PropTypes.string.isRequired,
-  renderInternal: PropTypes.func.isRequired,
-  renderExternal: PropTypes.func.isRequired
 }
 
 export default BCorpLink
