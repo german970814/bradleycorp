585fe74fd00c98da159f768eb85dba46c2c546a9
mattwills8 <matt_wills8@hotmail.co.uk>
2018-07-10 12:02:31 +0200

    add trademark link and fix footer styling

--- src/apps/Bradley/Footer/Copyright/Copyright.js
+++ src/apps/Bradley/Footer/Copyright/Copyright.js
@@ -5,37 +5,34 @@ import style from './Copyright.scss'
 const Copyright = props => {
   return (
     <React.Fragment>
+      <div className={style.divider} />
 
-      <div className={style.divider}></div>
-
-      <div className={`row ${style.copyrightWrapper}`} >
-
-        <div className={`col1 col2-tablet ${style.copyrightCol}`} >
-          <div className={`legal ${style.menuItem}`} >
+      <div className={`row ${style.copyrightWrapper}`}>
+        <div className={`col1 col2-tablet ${style.copyrightCol}`}>
+          <div className={`legal ${style.menuItem}`}>
             {`${String.fromCharCode(169)} 2018 Bradley Corporation`}
           </div>
-          <div className={`legal ${style.menuItem}`} >
-            <Link
-              to={'/legal'} >
-              {'Legal Statement'}
-            </Link>
-          </div>
-          <div className={`legal ${style.menuItem}`} >
-            <Link
-              to={'/privacy'} >
-              {'Privacy Policy'}
-            </Link>
-          </div>
+          <Link to={'/legal'} className={style.menuItem}>
+            <div className={`legal`}>{'Legal Statement'}</div>
+          </Link>
+          <Link to={'/privacy'} className={style.menuItem}>
+            <div className={`legal`}>{'Privacy Policy'}</div>
+          </Link>
         </div>
 
-        <div className={`col1 col2-tablet ${style.colRight} ${style.copyrightCol}`} >
-          <div className={`legal ${style.menuItem}`} >
-            {`The Bradley Logo and registered trademarks are owned by The Bradley Corporation${String.fromCharCode(174)}`}
-          </div>
+        <div
+          className={`col1 col2-tablet ${style.colRight} ${
+            style.copyrightCol
+          }`}>
+          <Link to="/trademarks">
+            <div className={`legal ${style.menuItem}`}>
+              {`The Bradley Logo and registered trademarks are owned by The Bradley Corporation${String.fromCharCode(
+                174
+              )}`}
+            </div>
+          </Link>
         </div>
-
       </div>
-
     </React.Fragment>
   )
 }
