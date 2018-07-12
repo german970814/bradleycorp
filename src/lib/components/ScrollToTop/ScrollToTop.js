38bf59547c83647e72d0ae21e3ec3441e5459e5e
mattwills8 <matt_wills8@hotmail.co.uk>
2018-07-12 12:42:53 +0200

    stop window from scrolling to the top on 'LoadMore' pagination

--- src/lib/components/ScrollToTop/ScrollToTop.js
+++ src/lib/components/ScrollToTop/ScrollToTop.js
@@ -15,7 +15,15 @@ type Props = {
 
 class ScrollToTop extends React.Component<Props> {
   componentDidUpdate (prevProps) {
-    if (this.props.location !== prevProps.location) {
+    // Scroll to top when location changes,
+    // but only if the pathname doesnt include pagination.
+    //
+    // This stops the window from scrolling to the top
+    // when we click a 'Load More'
+    if (
+      this.props.location !== prevProps.location &&
+      this.props.location.pathname.indexOf('/page=') === -1
+    ) {
       window.scroll({ top: 0, left: 0, behavior: 'smooth' })
     }
   }
