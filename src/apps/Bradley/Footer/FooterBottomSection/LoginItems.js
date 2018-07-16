a76e1c369d563e898f34500ee9e43b7db209509d
mattwills8 <matt_wills8@hotmail.co.uk>
2018-07-16 12:45:52 +0200

    add q20 footer bottom link

--- src/apps/Bradley/Footer/FooterBottomSection/LoginItems.js
+++ src/apps/Bradley/Footer/FooterBottomSection/LoginItems.js
@@ -5,23 +5,27 @@ import style from '../Footer.scss'
 const LoginItems = props => {
   return (
     <React.Fragment>
-
       <div className={'col1'}>
         <div className={`${style.menuItem} ${style.menuItemBottomSection}`}>
-          <div className={`small-link-gray ${style.menuItemLinkGray}`}>LOGIN</div>
+          <div className={`small-link-gray ${style.menuItemLinkGray}`}>
+            LOGIN
+          </div>
         </div>
       </div>
 
       <FooterBottomSectionItem
         link={'https://answernet.bradleycorp.com/'}
         title={'ANSWERNET'}
-        padlock />
+        padlock
+      />
 
       <FooterBottomSectionItem
         link={'http://bradzone.bradleycorp.com/'}
         title={'BRADZONE'}
-        padlock />
+        padlock
+      />
 
+      <FooterBottomSectionItem link={'/q2o'} title={'Q2O'} />
     </React.Fragment>
   )
 }
